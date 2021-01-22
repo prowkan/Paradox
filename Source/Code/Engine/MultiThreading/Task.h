9eb5c4de8a7ead7849686247f8da9ad8575785af
Victor <prowkan@gmail.com>
2021-01-22 10:07:50 +0700

    Убрал необходимость вызова Finish в методе Execute.

--- Source/Code/Engine/MultiThreading/Task.h
+++ Source/Code/Engine/MultiThreading/Task.h
@@ -8,11 +8,10 @@ class Task
 
 		virtual void Execute(const UINT ThreadID) = 0;
 
+		void Finish() { FinishFlag.store(true, memory_order::memory_order_seq_cst); }
 		void WaitForFinish() { while (!FinishFlag.load(memory_order::memory_order_seq_cst)); }
 
 	protected:
-
-		void Finish() { FinishFlag.store(true, memory_order::memory_order_seq_cst); }
 
 	private:
 
