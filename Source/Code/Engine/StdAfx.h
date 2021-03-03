a039f92e01beec16f8d6d86b374936d405a6ab6c
Victor <prowkan@gmail.com>
2021-03-03 22:20:46 +0700

    Обновил версию Vulkan SDK.

--- Source/Code/Engine/StdAfx.h
+++ Source/Code/Engine/StdAfx.h
@@ -14,8 +14,8 @@
 #include <DbgHelp.h>
 
 #define VK_USE_PLATFORM_WIN32_KHR
-#include <C:/VulkanSDK/1.2.162.0/Include/vulkan/vulkan.h>
-#include <C:/VulkanSDK/1.2.162.0/Include/shaderc/shaderc.h>
+#include <C:/VulkanSDK/1.2.162.1/Include/vulkan/vulkan.h>
+#include <C:/VulkanSDK/1.2.162.1/Include/shaderc/shaderc.h>
 
 #include <DirectXMath.h>
 
@@ -24,8 +24,8 @@
 
 #pragma comment(lib, "DbgHelp.lib")
 
-#pragma comment(lib, "C:/VulkanSDK/1.2.162.0/Lib/vulkan-1.lib")
-#pragma comment(lib, "C:/VulkanSDK/1.2.162.0/Lib/shaderc_shared.lib")
+#pragma comment(lib, "C:/VulkanSDK/1.2.162.1/Lib/vulkan-1.lib")
+#pragma comment(lib, "C:/VulkanSDK/1.2.162.1/Lib/shaderc_shared.lib")
 
 
 #ifdef _DEBUG
