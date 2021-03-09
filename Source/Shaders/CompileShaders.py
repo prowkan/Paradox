37c721dcb7e62df6b16722499b49f88d1a13853d
Victor <prowkan@gmail.com>
2021-03-09 14:43:00 +0700

    Переделал систему компиляции шейдеров.

---
+++ Source/Shaders/CompileShaders.py
@@ -0,0 +1,6 @@
+import subprocess
+
+FXCompiler = "C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe"
+
+subprocess.run([FXCompiler, "-T", "vs_5_1", "-E", "VS", "-Zpr", "-Fo", "./../../Build/GameContent/Shaders/MaterialBase_VertexShader.dxbc", "MaterialBase_VertexShader.hlsl"])
+subprocess.run([FXCompiler, "-T", "ps_5_1", "-E", "PS", "-Zpr", "-Fo", "./../../Build/GameContent/Shaders/MaterialBase_PixelShader.dxbc", "MaterialBase_PixelShader.hlsl"])
