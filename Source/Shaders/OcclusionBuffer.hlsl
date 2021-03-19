a71fa853f278300ed00cfce745f7c9e40120c32f
Victor <prowkan@gmail.com>
2021-03-19 22:19:14 +0700

    Исправил шейдер DownSampling'а Z-буфера.

--- Source/Shaders/OcclusionBuffer.hlsl
+++ Source/Shaders/OcclusionBuffer.hlsl
@@ -12,7 +12,7 @@ float PS(PSInput PixelShaderInput) : SV_Target
 {
 	float2 Offset = float2(1.0f / (2.0f * 256.0f * 144.0f), 1.0f / (2.0f * 256.0f * 144.0f));
 
-	float4 MinDepth, MinDepths;
+	/*float4 MinDepth, MinDepths;
 
 	MinDepths = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(Offset.x, Offset.y));
 	MinDepth.x = min(min(MinDepths.x, MinDepths.y), min(MinDepths.z, MinDepths.w));
@@ -21,7 +21,14 @@ float PS(PSInput PixelShaderInput) : SV_Target
 	MinDepths = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(Offset.x, -Offset.y));
 	MinDepth.z = min(min(MinDepths.x, MinDepths.y), min(MinDepths.z, MinDepths.w));
 	MinDepths = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(-Offset.x, -Offset.y));
-	MinDepth.w = min(min(MinDepths.x, MinDepths.y), min(MinDepths.z, MinDepths.w));
+	MinDepth.w = min(min(MinDepths.x, MinDepths.y), min(MinDepths.z, MinDepths.w));*/
 
+	float4 MinDepth;
+
+	MinDepth.x = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(Offset.x, Offset.y));
+	MinDepth.y = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(-Offset.x, Offset.y));
+	MinDepth.z = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(Offset.x, -Offset.y));
+	MinDepth.w = DepthBufferTexture.Sample(MinSampler, PixelShaderInput.TexCoord + float2(-Offset.x, -Offset.y));
+	
 	return min(min(MinDepth.x, MinDepth.y), min(MinDepth.z, MinDepth.w));
 }
