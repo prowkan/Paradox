1c78c68c36fdc15e90a99880f756608fbbc86984
Victor <prowkan@gmail.com>
2021-04-19 17:39:45 +0700

    Сделал классы заготовки для Render Pass'ов.

---
+++ Source/Code/Engine/Render/RenderPasses/FullScreenPass.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../RenderPass.h"
+
+class FullScreenPass : public RenderPass
+{
+
+};
---
+++ Source/Code/Engine/Render/RenderPasses/ScenePass.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../RenderPass.h"
+
+class ScenePass : public RenderPass
+{
+
+};
